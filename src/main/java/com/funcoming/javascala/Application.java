48cf79d65b25e3d247c875ad14eaf820ed18818f
GuoLiuFang <guoliufangopen@163.com>
2017-05-06 21:26:34 +0800

    提交标准 Java和 Scala 程序，以后以这个 commit 为标准模板

---
+++ src/main/java/com/funcoming/javascala/Application.java
@@ -0,0 +1,10 @@
+package com.funcoming.javascala;
+
+/**
+ * Created by LiuFangGuo on 5/6/17.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("草拟吗");
+    }
+}
